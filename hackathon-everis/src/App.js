59d9ed2b9121d0143fd32a7a65f320e308d51250
tanimikyu <bloergesh@gmail.com>
2021-07-19 16:02:19 -0400

    router

--- hackathon-everis/src/App.js
+++ hackathon-everis/src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router, Switch, Route, } from "react-router-dom";
 import './App.css';
+import { Login } from "./views/Login";
+import { Register } from "./views/Register";
+import { Home } from "./views/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+    <div className="container">
+        <Switch> 
+          <Route path="/" exact>
+              <Login />
+            </Route>
+          <Route path="/register">
+              <Register />
+            </Route>
+            <Route path="/home">
+              <Home />
+            </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
