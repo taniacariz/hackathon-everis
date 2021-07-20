6656bc4fc493c5b48c89143c663e21c1fa9f5456
tanimikyu <bloergesh@gmail.com>
2021-07-20 11:15:06 -0400

    changes login view

--- hackathon-everis/src/components-login/BodyLogin.jsx
+++ hackathon-everis/src/components-login/BodyLogin.jsx
@@ -2,8 +2,19 @@ import React from 'react'
 
 export const BodyLogin = () => {
     return (
-        <div>
-            
+             <div className="container-body">
+            <div className="container-img"></div>
+            <div className="container-form">
+                <form>
+                <h2 className="text-form">Iniciar sesión</h2>
+                <label>
+                    <input className="form-placeholder" type="text" name="name" placeholder="Ingresa tu correo Everis"/>
+                    <input className="form-placeholder" type="text" name="password" placeholder="Ingresa tu contraseña"/>
+                </label>
+                <div className="text-form">¿Olvidaste tu contraseña?</div>
+                    <input className="btn-submit" type="submit" value="INGRESAR" />
+                </form>
+            </div>
         </div>
     )
 }
--- hackathon-everis/src/components-login/FooterLogin.jsx
+++ hackathon-everis/src/components-login/FooterLogin.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export const FooterLogin = () => {
     return (
-        <footer>
-            
-        </footer>
+    <footer>
+        Desarrollado por “The Reactive Girls” - Laboratoria SCL016
+    </footer>
     )
 }
