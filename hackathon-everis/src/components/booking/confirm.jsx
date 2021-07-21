3dac7041213421d03bd06bdb337b29f10953ef35
k-garrido <katherinne.g@hotmail.com>
2021-07-21 15:17:32 -0500

    avanzando en la ventana de confirmacion

--- hackathon-everis/src/components/booking/confirm.jsx
+++ hackathon-everis/src/components/booking/confirm.jsx
@@ -1,11 +1,17 @@
 import React from "react";
+import { auth } from "../../Firebase.js";
 import ReturnArrow from "../home/Return-arrow";
 
 const Confirm = ({ data }) => {
   return (
     <div>
-      <button onClick={() => console.log(data)}>Veo Data</button>
-      Estas en confirmacion
+      <h1>Verifica los datos de tu visita a continuación:</h1>
+      <p>
+        Tu bloque de trabajo será programado para el día {data.date}
+        en el horario {data.timeblock} para {data.quantity} persona en el area{" "}
+        {data.area.area}
+      </p>
+      <button onClick={() => console.log(data)}>Confirmar</button>
       <ReturnArrow />
     </div>
   );
