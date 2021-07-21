7725bf955ce9ff750ee8b45285db63742db983d2
tanimikyu <bloergesh@gmail.com>
2021-07-21 10:41:47 -0400

    no sé qué quiere cambiar esta wea uwu

--- hackathon-everis/src/components/login/Hour.jsx
+++ hackathon-everis/src/components/login/Hour.jsx
@@ -3,7 +3,6 @@ import moment from "moment";
 import "moment/locale/es";
 
 const Hour = () => {
-  
   const today = new Date();
 
   return <div className="nav-hour">{moment(today).format("LLL")}</div>;
--- hackathon-everis/src/views/Bookings.jsx
+++ hackathon-everis/src/views/Bookings.jsx
@@ -2,18 +2,20 @@ import React, { useContext, useReducer } from "react";
 import NavTop from "../components/home/NavTop";
 import Date from "../components/booking/date";
 import Hour from "../components/booking/hour";
-import Quantity from "../components/booking/quantity";
 import Workspace from "../components/booking/work-space";
 import Confirm from "../components/booking/confirm";
 import { FormContext } from "../context/form-context";
-
 
 const renderCurrentPage = (step, formData, dispatch) => {
   switch (step) {
     case 1:
       return <Date dispatch={(value) => dispatch({ date: value })} />;
     case 2:
-      return <Hour dispatch={(value) => dispatch({ timeblock: value, quantity: value })} />;
+      return (
+        <Hour
+          dispatch={(value) => dispatch({ timeblock: value, quantity: value })}
+        />
+      );
     case 3:
       return <Workspace dispatch={(value) => dispatch({ area: value })} />;
     case 4:
